b437beee9f8f4c5e9dd43e9ffd252daea85d3df2
Henry Osadzinski <henry@osadzinski.co.uk>
2015-07-14 16:41:34 +0100

    added datestamp to todos

--- js/models/todo.js
+++ js/models/todo.js
@@ -6,11 +6,21 @@ var app = app || {};
 // ----------
 // Our basic **Todo** model has `title` and `completed` attributes.
 
+
+var createdDate = new Date();
+var dd = createdDate.getDate();
+var mm = createdDate.getMonth()+1;
+var yy = createdDate.getFullYear().toString().substr(2,2);
+if (dd<10){dd = '0' + dd;}
+if (mm<10){mm = '0' + mm;}
+
 app.Todo = Backbone.Model.extend({
+
 
   // Default attributes ensure that each todo created has `title` and `completed` keys.
   defaults: {
     title: '',
+    created: dd + '/' + mm + '/' + yy,
     completed: false
   },
 
